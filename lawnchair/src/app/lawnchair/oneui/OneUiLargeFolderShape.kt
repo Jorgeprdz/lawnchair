7dda63c467e525c7905c423aafac8ab8852c83f6
Jorgeprdz <JORGEPALACIOSRODRIGUEZ@GMAIL.COM>
2026-09-10 11:08:04 -0600

    fix: define large folder shape values

---
+++ lawnchair/src/app/lawnchair/oneui/OneUiLargeFolderShape.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2026 Lawnchair
+ * Licensed under the Apache License, Version 2.0
+ */
+package app.lawnchair.oneui
+
+/** Stable values persisted by [OneUiGlassPreferences] for the 2x2 large-folder preview. */
+object OneUiLargeFolderShape {
+    const val ROUNDED_SQUARE = 0
+    const val CIRCLE = 1
+}
